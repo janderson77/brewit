3f9a96a902bc5cf71b8731838018dbee7f724fb0
janderson77 <jamesseventyseven@gmail.com>
2022-02-13 17:52:28 -0700

    Added packages, helpers and register route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,6 +6,27 @@ const { validate } = require("jsonschema");
 
 const {userNewSchema} = require("../schemas/index");
 
+const createToken = require("../helpers/createToken")
+
 router.post("/register", async function(req, res, next){
-    
+    try{
+        if(req.body._token){
+            delete req.body._token
+        }
+        const isValid = validate(req.body, userNewSchema);
+
+        if(!isValid.valid){
+            return next({
+                status: 400,
+                message: isValid.errors.map(e => e.stack)
+            });
+        };
+
+        const newUser = await User.register(req.body);
+        const token = createToken(newUser);
+        newUser._token = token;
+        return res.status(201).json(newUser);
+    }catch(e){
+        return next(e)
+    }
 })
