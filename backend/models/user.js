f56637b76bb3b485ed29b661582dd07c44ec0c26
janderson77 <jamesseventyseven@gmail.com>
2022-08-14 16:02:29 -0600

    Added Passport

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
     email: {
         type: String,
         trim: true,
         lowercase: true,
         unique: true,
-        required: [true, "Please enter an email address"],
-        match: [/.+\@.+\..+/, "Please enter a valid email address"],
+        required: [true, "Please enter an email address."],
+        match: [/.+\@.+\..+/, "Please enter a valid email address."],
         unique: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        required: [true, "Please enter a username."],
+        unique: [true, "This username is already in use."]
     }
-})
+});
+
+module.exports = mongoose.model('User', userSchema)
