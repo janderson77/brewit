378e78866bd388d641296f1cb216d80b5bb95c63
janderson77 <jamesseventyseven@gmail.com>
2022-08-15 19:00:01 -0600

    removed unused imports, directory, and file

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require('../middleware/auth')
-
-const User = require("../models/user");
-const { validate } = require("jsonschema");
-
-const {userNewSchema, userAuthSchema} = require("../schemas/index");
-
-const createToken = require("../helpers/createToken");
 const passport = require("passport");
 
 router.post("/register", authMiddleware.register, authMiddleware.signJWTForUser, (req, res, next) => {});
